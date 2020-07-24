2fdeb162c6b7c7b5fac2de8ae6bed553fb7f95d1
leowirasanto-moka <leo.laia@mokapos.com>
2020-07-24 15:46:52 +0700

    Initial Commit

---
+++ GetToKnowSQLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetToKnowSQLite
+//
+//  Created by MOKA on 24/07/20.
+//  Copyright © 2020 leowirasanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
